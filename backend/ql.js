466a58d12a1d8a64cc030ada34e62c3a36eefac0
thek <1191876950@qq.com>
2023-03-10 18:02:34 +0800

    位置修正

--- backend/ql.js
+++ backend/ql.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 const { readFile } = require('fs/promises');
 const path = require('path');
 
-const qlDir = process.env.QL_DIR || '/ql/data';
-const authFile = path.join(qlDir, 'config/auth.json');
+const qlDir = process.env.QL_DIR || '/ql';
+const authFile = path.join(qlDir, '/data/config/auth.json');
 
 const api = got.extend({
   prefixUrl: process.env.QL_URL || 'http://localhost:5600',
@@ -14,7 +14,7 @@ const api = got.extend({
 });
 
 async function getToken() {
-  const authConfig = JSON.parse(await readFile('/ql/data/config/auth.json'));
+  const authConfig = JSON.parse(await readFile(authFile));
   return authConfig;
 }
 
